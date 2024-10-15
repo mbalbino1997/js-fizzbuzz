f62d727b6b5b55ca60b78b5161147dd1669da223
mbalbino1997 <rmassimo.balbino@gmail.com>
2024-10-15 16:19:32 +0200

    add:FizzBuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,14 @@
+for(let i = 0; i<100; i++) {
+    let num = i+1;
+    let rest3 = num%3===0;
+    let rest5 = num%5===0;
+    if(rest3 && rest5) {
+        console.log("FizzBuzz",);
+    } else if(rest3) {
+        console.log("Fizz");
+    } else if(rest5) {
+        console.log("Buzz");
+    } else {
+        console.log(num);
+    }
+}
